d7de9695b529264e0abedb365a799aecf335af20
Austin Mullen <mullen@localhost.localdomain>
2020-08-13 16:46:17 -0700

    Removed some more leftover files

--- bonibdbatch.sh
+++
@@ -1,5 +0,0 @@
-for i in 0.12 0.15 0.17 0.20
-  do
-    bonsai "WLSP_"$i"_pccibd.root" "WLSP_"$i"_pccibd_bon.root"
-    bonsai "BASE_"$i"_pccibd.root" "BASE_"$i"_pccibd_bon.root"
-  done
--- bwrun.sh
+++
@@ -1,4 +0,0 @@
-rat ~/Software/ratpac/mac/WLSP_run.mac 
-rat ~/Software/ratpac/mac/WLSP_baserun.mac
-bonsai WLSP_notarp.root WLSP_notarp_bon.root
-bonsai BASE_notarp.root BASE_notarp_bon.root
